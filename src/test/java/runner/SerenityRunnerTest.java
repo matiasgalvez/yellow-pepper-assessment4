d3b0483838d35cfcc539192edc197e95a8b1586e
Matias.Galvez <mattg1593@gmail.com>
2024-05-29 21:33:30 -0300

    first working commit

---
+++ src/test/java/runner/SerenityRunnerTest.java
@@ -0,0 +1,11 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(plugin ="pretty",features= {"src/test/resources/features"}, glue= {"steps"})
+
+public class SerenityRunnerTest {
+}
